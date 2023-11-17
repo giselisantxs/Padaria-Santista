0151a5f6bbc0c898604635f5708f9cd0c9edae40
Ryhan07 <sadninjaa2004@gmail.com>
2023-11-16 21:21:04 -0300

    padoca gigiba
    foi ate aula php

---
+++ aula1.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Giseli Santos" ;
+
+$cidade = "São Paulo";
+
+$curso = "Programador de Web";
+
+$local = "Obra Social Do bosco";
+
+$escolaridade = "Ensino medio completo";
+
+$time = "Santastico";
+
+echo "Meu nome é $nome e moro em $cidade faço $curso na $local temho $escolaridade torço para o maior time do mundo $time "
+
+?>
